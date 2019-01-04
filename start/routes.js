50a955507d114cf4729cd38f65505c11db258bf4
Jonthan Boyer <contact@grafikart.fr>
2019-01-04 01:26:58 +0100

    feat(views): link to the doc

--- start/routes.js
+++ start/routes.js
@@ -26,7 +26,12 @@ Route.get('/', () => {
       <section>
         <div class="logo"></div>
         <div class="title"></div>
-        <div class="subtitle"><p>AdonisJs simplicity will make you feel confident about your code</p></div>
+        <div class="subtitle">
+          <p>AdonisJs simplicity will make you feel confident about your code</p>
+          <p>
+            Don't know where to start? Read the <a href="https://adonisjs.com/docs">documentation</a>.
+          </p>    
+      </div>
       </section>
     </body>
   </html>
